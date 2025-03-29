cf025bc53ab15f08b960b720572dc3ffbad26fb5
Tucker Hill <68754392+hill1635@users.noreply.github.com>
2025-03-29 14:04:27 -0600

    Added start dialog view.

--- games/coin_flip.js
+++ games/coin_flip.js
@@ -1,6 +1,17 @@
+let eventLoop = require("event_loop");
+let gui = require("gui");
 let math = require("math");
+let dialogView = require("gui/dialog");
 let choices = ["Heads", "Tails"];
 const COIN_POSITIONS = ["--", "\\", "|", "/"];
+
+let views = {
+    startDialog: dialogView.makeWith({
+        header: "Coin Flipper",
+        text: "Flip a coin",
+        center: "Flip",
+    }),
+};
 
 function getRandomInt(min, max) {
     return math.floor(math.random() * (max - min)) + min;
@@ -31,4 +42,13 @@ function flipCoin() {
     pickRandomChoice();
 }
 
-flipCoin();
+eventLoop.subscribe(views.startDialog.input, function (_sub, button, gui, views) {
+    if (button === "center") {
+        flipCoin();
+    }
+}
+, gui, views);
+
+// flipCoin();
+gui.viewDispatcher.switchTo(views.startDialog);
+eventLoop.run();
